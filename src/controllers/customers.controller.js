2601315ca90b5390e130658997631f5f91ab7a63
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-11 07:36:42 -0300

    feat: get customers

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,26 +1,27 @@
-
-export function getCustomersController (req,res){
+import db from '../database/db.js'
+export async function getCustomersController (req,res){
+    try {
+        const customers = await db.query(`select * from customers;`)
+        res.send(customers.rows)
+    } catch (e) {
+        res.status(500).send({ error: "Problemas no servidor."})
+    }
+}
+export async function getCustomersByIDController (req,res){
     try {
         
     } catch (e) {
         
     }
 }
-export function getCustomersByIDController (req,res){
+export async function createCustomersController (req,res){
     try {
         
     } catch (e) {
         
     }
 }
-export function createCustomersController (req,res){
-    try {
-        
-    } catch (e) {
-        
-    }
-}
-export function updateCustomersByIDController (req,res){
+export async function updateCustomersByIDController (req,res){
     try {
         
     } catch (e) {
