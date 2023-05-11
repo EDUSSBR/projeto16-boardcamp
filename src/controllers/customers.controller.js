4b8fac6640497d817474b0f1a5f4705d3938baa9
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-11 10:01:33 -0300

    feat: get customer by id

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -9,9 +9,14 @@ export async function getCustomersController (req,res){
 }
 export async function getCustomersByIDController (req,res){
     try {
-        
+        const { id } = req.params
+        const customers =  await db.query(`SELECT * FROM customers WHERE id=$1;`, [id])
+        if(customers.rowCount===0){
+            return res.status(404).send()
+        }
+        res.send(customers.rows[0])
     } catch (e) {
-        
+        res.status(400).send()
     }
 }
 export async function createCustomersController (req,res){
@@ -31,8 +36,12 @@ export async function createCustomersController (req,res){
 }
 export async function updateCustomersByIDController (req,res){
     try {
-        
+        const { id } = req.params
+        console.log(id)
+        // const customers =  await db.query(`SELECT name FROM customers WHERE cpf=$1;`, [cpf])
+        res.send(id)
     } catch (e) {
+        res.status(400).send(e)
         
     }
 }
