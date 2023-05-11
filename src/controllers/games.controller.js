9ec0888a2eef2c977343b68c2d5a7342e001fdae
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-11 08:11:24 -0300

    feat: add game insertion controller logic with validation

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -5,17 +5,21 @@ export async function getGamesController(req, res) {
         const games = await db.query(`select * from games;`)
         res.send(games.rows)
     } catch (e) {
-        res.status(500).send({ error: "Problemas no servidor."})
+        res.status(500).send({ error: "Problemas no servidor." })
     }
 }
 export async function createGamesController(req, res) {
     try {
-
+        const { name, image, stockTotal, pricePerDay } = req.body
+        
+        const games =  await db.query(`SELECT name FROM games WHERE name=$1;`, [name])
+        if (games.rowCount>0){
+            return res.status(409).send()
+        }
+        await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay])
+        res.status(201).send()
     } catch (e) {
-
+        console.log(e)
+        res.status(400).send()
     }
 }
-
-
-// gameRouter.get('/games', getGamesController)
-// gameRouter.post('/games', createGamesController)
--- src/routers/games.route.js
+++ src/routers/games.route.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { getGamesController, createGamesController } from '../controllers/games.controller.js';
+import { validateSchema } from '../middlewares/validateSchema.js';
+import { gameSchema } from '../schemas/gamesSchema.js';
 
 const gameRouter = Router()
 
-gameRouter.get('/games', getGamesController)
-gameRouter.post('/games', createGamesController)
+gameRouter.get('/games',  getGamesController)
+gameRouter.post('/games', validateSchema(gameSchema), createGamesController)
 
 export { gameRouter };
