176b66c663780058e123bb25c3b09fe315c32987
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-12 09:26:23 -0300

    feat: querystring search with name for games

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -2,18 +2,25 @@ import db from '../database/db.js'
 
 export async function getGamesController(req, res) {
     try {
-        const games = await db.query(`select * from games;`)
+        const { name } = req.query
+        let games;
+        if (name){
+            games = await db.query(`SELECT * FROM games WHERE name ILIKE $1||'%';`,[name])
+        } else {
+            games =await db.query(`SELECT * FROM games;`)
+        }
         res.send(games.rows)
     } catch (e) {
+        console.log(e)
         res.status(500).send({ error: "Problemas no servidor." })
     }
 }
 export async function createGamesController(req, res) {
     try {
         const { name, image, stockTotal, pricePerDay } = req.body
-        
-        const games =  await db.query(`SELECT name FROM games WHERE name=$1;`, [name])
-        if (games.rowCount>0){
+
+        const games = await db.query(`SELECT name FROM games WHERE name=$1;`, [name])
+        if (games.rowCount > 0) {
             return res.status(409).send()
         }
         await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay])
