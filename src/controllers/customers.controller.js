2be279ae696e21fe20c2ca8495b1e20be8b41d43
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-12 11:32:25 -0300

    feat: add ordination for customers searching

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,31 +1,39 @@
 import db from '../database/db.js'
-export async function getCustomersController (req,res){
+export async function getCustomersController(req, res) {
     try {
-        let { cpf, limit, offset } = req.query
+        let { cpf, limit, offset, order, desc } = req.query
+        const customersColumns = ['id', 'name', 'phone', 'cpf', 'birthday'];
         cpf = cpf || "";
         offset = offset || 0;
         limit = Number(limit) || null;
         let customers;
-        if (cpf || offset || limit){
-            customers = await db.query(`
+        const customersColumnExists = customersColumns.some(item => item === order)
+        if (cpf || offset || limit || customersColumnExists || desc === "DESC") {
+            let query = `
             SELECT * FROM customers 
             WHERE cpf LIKE $1||'%'
             OFFSET $2
             LIMIT $3
-            ;`,[cpf, offset, limit])
+            ;`
+            if (customersColumnExists){
+                query+=` ORDER BY ${order} ${desc === "true" ? "DESC" : "ASC"}`
+            }
+            customers = await db.query(query, [cpf, offset, limit])
+
+
         } else {
-            customers =await db.query(`SELECT * FROM customers;`)
+            customers = await db.query(`SELECT * FROM customers;`)
         }
         res.send(customers.rows)
     } catch (e) {
-        res.status(500).send({ error: "Problemas no servidor."})
+        res.status(500).send({ error: "Problemas no servidor." })
     }
 }
-export async function getCustomersByIDController (req,res){
+export async function getCustomersByIDController(req, res) {
     try {
         const { id } = req.params
-        const customers =  await db.query(`SELECT * FROM customers WHERE id=$1;`, [id])
-        if(customers.rowCount===0){
+        const customers = await db.query(`SELECT * FROM customers WHERE id=$1;`, [id])
+        if (customers.rowCount === 0) {
             return res.status(404).send()
         }
         res.send(customers.rows[0])
@@ -33,34 +41,34 @@ export async function getCustomersByIDController (req,res){
         res.status(400).send()
     }
 }
-export async function createCustomersController (req,res){
+export async function createCustomersController(req, res) {
     try {
         const { name, phone, cpf, birthday } = req.body
-        
-        const customers =  await db.query(`SELECT name FROM customers WHERE cpf=$1;`, [cpf])
-        if (customers.rowCount>0){
+
+        const customers = await db.query(`SELECT name FROM customers WHERE cpf=$1;`, [cpf])
+        if (customers.rowCount > 0) {
             return res.status(409).send()
         }
-        await db.query(`INSERT INTO customers (name, phone, cpf, birthday ) VALUES ($1, $2, $3, $4);`, [name, phone, cpf, birthday ])
+        await db.query(`INSERT INTO customers (name, phone, cpf, birthday ) VALUES ($1, $2, $3, $4);`, [name, phone, cpf, birthday])
         res.status(201).send()
     } catch (e) {
         console.log(e)
         res.status(400).send()
     }
 }
-export async function updateCustomersByIDController (req,res){
+export async function updateCustomersByIDController(req, res) {
     try {
         const { name, phone, cpf, birthday } = req.body
         const { id } = req.params
-        const cpfInfo =  await db.query(`SELECT cpf FROM customers WHERE cpf=$1 and id<>$2;`, [cpf, id])
+        const cpfInfo = await db.query(`SELECT cpf FROM customers WHERE cpf=$1 and id<>$2;`, [cpf, id])
         const cpfExists = cpfInfo.rowCount > 0
-        if(cpfExists){
+        if (cpfExists) {
             return res.status(409).send()
-        } 
-        await db.query(`UPDATE customers SET name=$1,phone=$2,cpf=$3,birthday=$4 WHERE id=$5;`, [name, phone, cpf, birthday,id])
+        }
+        await db.query(`UPDATE customers SET name=$1,phone=$2,cpf=$3,birthday=$4 WHERE id=$5;`, [name, phone, cpf, birthday, id])
         res.send("passou")
     } catch (e) {
         res.status(400).send(e)
-        
+
     }
 }
